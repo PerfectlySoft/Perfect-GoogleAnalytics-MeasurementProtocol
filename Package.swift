4ee8889352e111f13c17166f54e3e4c0d7a892ea
Jonathan Guthrie <jono@perfect.org>
2017-06-21 14:54:36 -0400

    update minor versions

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ let package = Package(
     name: "Perfect-GoogleAnalytics-MeasurementProtocol",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2, minor: 0),
-        .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2),
+        .Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2),
+        .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
     ]
 )
