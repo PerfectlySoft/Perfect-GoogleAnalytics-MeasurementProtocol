87e36c72c923f09bbdf0eadb3597883e38ae3b73
Kyle Jessup <kyle.jessup@treefrog.ca>
2019-03-26 13:57:43 -0400

    Updated package file

--- Package.swift
+++ Package.swift
@@ -1,10 +1,16 @@
+// swift-tools-version:4.2
 // Generated automatically by Perfect Assistant Application
 // Date: 2017-10-20 01:00:46 +0000
 import PackageDescription
 let package = Package(
 	name: "Perfect-GoogleAnalytics-MeasurementProtocol",
-	targets: [],
+	products: [
+		.library(name: "Perfect-GoogleAnalytics-MeasurementProtocol", targets: ["Perfect-GoogleAnalytics-MeasurementProtocol"])
+	],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 3),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", from: "3.0.0"),
+	],
+	targets: [
+		.target(name: "Perfect-GoogleAnalytics-MeasurementProtocol", dependencies: ["PerfectCURL"])
 	]
 )
